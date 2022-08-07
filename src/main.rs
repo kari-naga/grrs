b8da6adb691b159846c151d743f1717714a6561a
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2022-08-07 17:47:12 -0400

    Fix IO redirection

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use clap::Parser;
 use std::fs::File;
 use std::io::{stdout, BufWriter, Write, BufReader, BufRead};
 use anyhow::{Context, Result};
-use terminal_size::{terminal_size};
+use terminal_size::{Width, terminal_size};
 use ansi_term::Colour::Red;
 use regex::Regex;
 
 #[derive(Parser)]
 #[clap(
-    version = "0.2.0",
-    about = "A simple program to search files for a given pattern",
+    version = env!("CARGO_PKG_VERSION"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
 )]
 struct Args {
     #[clap(help = "The pattern to search for")]
@@ -42,9 +42,17 @@ fn main() -> Result<()> {
     let file = File::open(&args.path)
         .with_context(|| format!("could not open file `{}`", &args.path.display()))?;
 
-    let (width, _) = terminal_size().with_context(|| "could not get terminal size")?;
-
     let lines = BufReader::new(file).lines().enumerate();
+
+    let mut in_terminal = true;
+
+    let width = match terminal_size() {
+        Some((Width(width), _)) => width as usize,
+        None => {
+            in_terminal = false;
+            120
+        },
+    };
 
     let re = Regex::new(&(
         if args.case_insensitive {"(?i)"} else {""}.to_string()
@@ -63,22 +71,22 @@ fn main() -> Result<()> {
         let match_result = re.is_match(&line);
         if if args.invert_match {!match_result} else {match_result} {
             if last_idx != 0 && idx - last_idx > 1 {
-                writeln!(handle, "{}", "-".repeat(width.0 as usize))
+                writeln!(handle, "{}", "-".repeat(width))
                     .with_context(|| "could not print separator")?;
             }
             let end = format!("line {}", idx);
-            let line = if line.len() > width.0 as usize - end.len() - 3 {
-                (&line[..width.0 as usize - end.len() - 6]).to_string() + "..."
+            let line = if line.len() > width - end.len() - 3 {
+                (&line[..width - end.len() - 6]).to_string() + "..."
             } else {
                 line
             };
             let len = line.len();
-            let line = if !args.invert_match && args.highlight_matches {
+            let line = if in_terminal && !args.invert_match && args.highlight_matches {
                 re.replace_all(&line, Red.bold().paint("$match").to_string()).to_string()
             } else {
                 line
             };
-            writeln!(handle, "{line}{}{end}", " ".repeat(width.0 as usize - len - end.len()))
+            writeln!(handle, "{line}{}{end}", " ".repeat(width - len - end.len()))
                 .with_context(|| format!("could not print line {idx} in file `{}`", &args.path.display()))?;
             last_idx = idx;
             count += 1;
@@ -89,7 +97,7 @@ fn main() -> Result<()> {
         writeln!(handle, "found no matches")
             .with_context(|| "could not print message")?;
     } else if args.count_matches {
-        writeln!(handle, "{}", "-".repeat(width.0 as usize))
+        writeln!(handle, "{}", "-".repeat(width))
             .with_context(|| "could not print separator")?;
         writeln!(handle, "found {count} match{}", if count > 1 {"es"} else {""})
             .with_context(|| "could not print message")?;
