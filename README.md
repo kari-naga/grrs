1fc632f97befaf48383e79a3e6b931e3baadfb5f
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2022-08-07 16:41:51 -0400

    Add regex functionality and fix bugs

---
+++ README.md
@@ -0,0 +1,11 @@
+# grrs
+
+A simple Rust command-line program to search files for a given pattern
+
+# Installation
+
+Run `cargo build --release` and add the resulting binary under `target/release` to your path.
+
+# Todo
+
+- Search folders
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ use std::io::{stdout, BufWriter, Write, BufReader, BufRead};
 use anyhow::{Context, Result};
 use terminal_size::{terminal_size};
 use ansi_term::Colour::Red;
+use regex::Regex;
 
 #[derive(Parser)]
 #[clap(
-    version = "0.1.1",
+    version = "0.2.0",
     about = "A simple program to search files for a given pattern",
 )]
 struct Args {
@@ -33,40 +34,6 @@ struct Args {
     count_matches: bool,
 }
 
-fn matches_pattern(line: &str, args: &Args) -> bool {
-    match (args.case_insensitive, args.match_words) {
-        (true, true) => line.split_whitespace().any(|s| s.to_lowercase() == args.pattern.to_lowercase()),
-        (_, true) => line.split_whitespace().any(|s| s == args.pattern),
-        (true, _) => line.to_lowercase().contains(&args.pattern.to_lowercase()),
-        (_, _) => line.contains(&args.pattern)
-    }
-}
-
-fn format(line: &String, args: &Args) -> String {
-    match (args.case_insensitive, args.match_words) {
-        (true, true) => line.split(" ")
-            .map(|s| if s.to_lowercase() == args.pattern.to_lowercase() {Red.bold().paint(s).to_string()} else {s.to_string()})
-            .collect::<Vec<_>>()
-            .join(" "),
-        (_, true) => line.split(" ")
-            .map(|s| if s == args.pattern {Red.bold().paint(s).to_string()} else {s.to_string()})
-            .collect::<Vec<_>>()
-            .join(" "),
-        (true, _) => {
-            let mut modified_line = line.clone();
-            let mut old_len = modified_line.len();
-            for (idx, _) in line.to_lowercase().match_indices(&args.pattern.to_lowercase()) {
-                let idx = idx + modified_line.len() - old_len;
-                let end = idx + args.pattern.len();
-                old_len = modified_line.len();
-                modified_line.replace_range(idx..end, &Red.bold().paint(&modified_line[idx..end]).to_string());
-            }
-            modified_line
-        },
-        (_, _) => line.replace(&args.pattern, &Red.bold().paint(&args.pattern).to_string())
-    }
-}
-
 fn main() -> Result<()> {
     let args = Args::parse();
 
@@ -82,20 +49,35 @@ fn main() -> Result<()> {
     let mut last_idx = 0;
 
     let mut count = 0;
+
+    let re = Regex::new(&(
+        if args.case_insensitive {"(?i)"} else {""}.to_string()
+        + if args.match_words {r"\b(?P<match>"} else {"(?P<match>"}
+        + &args.pattern
+        + if args.match_words {r")\b"} else {")"}
+    )).with_context(|| format!("could not compile regex for pattern `{}`", &args.pattern))?;
     
     for (idx, line) in lines {
         let idx = idx + 1;
         let line = line.with_context(|| format!("could not read line {idx} in file `{}`", &args.path.display()))?;
-        let match_result = matches_pattern(&line, &args);
+        let match_result = re.is_match(&line);
         if if args.invert_match {!match_result} else {match_result} {
             if last_idx != 0 && idx - last_idx > 1 {
                 writeln!(handle, "{}", "-".repeat(width.0 as usize))
                     .with_context(|| "could not print separator")?;
             }
             let end = format!("line {}", idx);
-            let line = if line.len() > width.0 as usize - end.len() - 3 {(&line[..width.0 as usize - end.len() - 6]).to_string() + "..."} else {line};
+            let line = if line.len() > width.0 as usize - end.len() - 3 {
+                (&line[..width.0 as usize - end.len() - 6]).to_string() + "..."
+            } else {
+                line
+            };
             let len = line.len();
-            let line = if !args.invert_match && args.highlight_matches {format(&line, &args)} else {line};
+            let line = if !args.invert_match && args.highlight_matches {
+                re.replace_all(&line, Red.bold().paint("$match").to_string()).to_string()
+            } else {
+                line
+            };
             writeln!(handle, "{line}{}{end}", " ".repeat(width.0 as usize - len - end.len()))
                 .with_context(|| format!("could not print line {idx} in file `{}`", &args.path.display()))?;
             last_idx = idx;
